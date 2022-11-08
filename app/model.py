62fe4fc59d2ce41cb7e4994c8664e7377637be59
Ankita Mahato <ankita.mahato@karkinos.in>
2022-11-08 11:13:55 +0530

    second commit

--- app/model.py
+++ app/model.py
@@ -2,6 +2,7 @@ from flask import Flask, render_template, request, url_for, redirect
 from app import app
 from flask_mysqldb import MySQL 
 import MySQLdb.cursors
+#from bs4 import BeautifulSoup
 
 from random import randint
   
@@ -15,12 +16,18 @@ app.config['MYSQL_DB'] = 'ncdpython'
 
 mysql = MySQL(app)
 
+def final_touch(field):
+    field = field.strip()
+    field = field.replace("\\", "")
+
+    #field = BeautifulSoup(field, convertEntities=BeautifulSoup.HTML_ENTITIES)
+    return field 
 
 
 @app.route('/search',methods=['GET', 'POST'])
 def search():
     
-    primary_key=""
+    pk=""
     pid=""
     records=()
     
@@ -28,32 +35,68 @@ def search():
 
         choice=request.form['choice']
         
-        primary_key=request.form['primary_key']
-        primary_key.capitalize()
+        pk=request.form['pk']
+        
+        
 
         cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
 
         if choice=='PATIENT ID':
-            if primary_key.isdigit():
-                cursor.execute("SELECT * from patient WHERE patient_id LIKE '%"+primary_key+"%' ;")
-                records=cursor.fetchall()
-                cursor.close()
+            if(not pk):
+                return render_template('search.html',error_display_patient_id="block",
+                error_patient_id="Error: Patient ID is required")
             else:
-                return render_template('search.html',error="Enter digits only") 
+                final_touch(pk)
+                if(pk.isspace()):
+                    return render_template('search.html',error_display_patient_id="block",
+                        error_patient_id="Error: Patient ID doesn't start with a SPACE")
+                
+                elif(len(pk)>14):
+                    return render_template('search.html',error_display_patient_id="block",
+                    error_patient_id="Error: Patient ID must be less than 14 digits")
+
+                else:
+                    if pk.isdigit():
+                        cursor.execute("SELECT * from patient WHERE patient_id LIKE '%"+pk+"%' ;")
+                        records=cursor.fetchall()
+                        cursor.close()
+                    else :
+                        return render_template('search.html',error="Enter digits only") 
         elif choice=='FIRST NAME':
-            if primary_key.isalpha():
-                cursor.execute("SELECT * from patient WHERE first_name LIKE '%"+primary_key+"%' ;")
-                records=cursor.fetchall()
-                cursor.close()
+            pk.capitalize()
+            if(not pk): 
+                return render_template('search.html',error_display_firstname="block",
+                    error_pk="Error: First Name is required")
             else:
-                return render_template('search.html',error="Enter alphabets only") 
+                final_touch(pk)
+                if(pk.isspace()):
+                    return render_template('search.html',error_display_firstname="block",
+                        error_firstname="Error: First Name doesn't start with a SPACE")
+                else:
+                    if (('^[a-zA-Z\s]+$', pk)):
+                        cursor.execute("SELECT * from patient WHERE first_name LIKE '%"+pk+"%' ;")
+                        records=cursor.fetchall()
+                        cursor.close()
+                    else:
+                        return render_template('search.html',error="Enter alphabets only") 
+
         elif choice=='LAST NAME':
-            if primary_key.isalpha():
-                cursor.execute("SELECT * from patient WHERE last_name LIKE '%"+primary_key+"%' ;")
-                records=cursor.fetchall()
-                cursor.close()
+            pk.capitalize()
+            if(not pk): 
+                return render_template('search.html',error_display_lastname="block",
+                    error_lastname="Error: First Name is required")
             else:
-                return render_template('search.html',error="Enter alphabets only") 
+                final_touch(pk)
+                if(pk.isspace()):
+                    return render_template('search.html',error_display_lastname="block",
+                        error_lastname="Error: Last Name doesn't start with a SPACE")
+                else:
+                    if pk.isalpha():
+                        cursor.execute("SELECT * from patient WHERE last_name LIKE '%"+pk+"%' ;")
+                        records=cursor.fetchall()
+                        cursor.close()
+                    else:
+                        return render_template('search.html',error="Enter alphabets only") 
 
     return render_template('search.html', records=records)
 
@@ -69,6 +112,10 @@ def making_global_patient(patientid):
 @app.route('/register', methods=['GET', 'POST'])
 def register():
 
+
+    error_name=error_username=error_password=error_gender =error_dob=error_head=""
+    error_display_name=error_display_username=error_display_password=error_display_gender =error_display_dob=error_display_head="none"
+    flag_error=0
     if request.method=="POST":
 
         firstname=""
@@ -85,16 +132,111 @@ def register():
 
         
         firstname=request.form['firstname']
+        firstname.capitalize()
         lastname=request.form['lastname']
+        lastname.capitalize()
         gender=request.form['gender']
         aadhaar=request.form['aadhaar']
         phone=request.form['phone']
         birthday=request.form['birthday']
         pincode=request.form['pincode']
+        
+        if(not firstname): 
+            flag_error=1
+            return render_template('registration.html',error_display_firstname="block",
+                    error_firstname="Error: First Name is required",lastname=lastname,gender=gender,aadhaar=aadhaar,
+                    phone=phone,birthday=birthday,pincode=pincode)
+        else:
+            final_touch(firstname)
+            if(firstname.isspace()):
+                flag_error=1
+                return render_template('registration.html',error_display_firstname="block",
+                        error_firstname="Error: First Name can't start with a SPACE",lastname=lastname,gender=gender,
+                        aadhaar=aadhaar,phone=phone,birthday=birthday,pincode=pincode)
+
+        
+        if(not lastname): 
+            flag_error=1
+            return render_template('registration.html',error_display_lastname="block",
+                    error_lastname="Error: Last Name is required",firstname=firstname,gender=gender,aadhaar=aadhaar,
+                    phone=phone,birthday=birthday,pincode=pincode)
+        else:
+            final_touch(lastname)
+            if(lastname.isspace()):
+                flag_error=1
+                return render_template('registration.html',error_display_lastname="block",
+                        error_lastname="Error: Last Name can't start with a SPACE",firstname=firstname,gender=gender,
+                        aadhaar=aadhaar,phone=phone,birthday=birthday,pincode=pincode)
+
+        
+        if(not gender):
+            flag_error=1
+            return render_template('registration.html',error_display_gender="block",
+                    error_gender="Error: Select your gender",
+                    firstname=firstname,lastname=lastname,aadhaar=aadhaar,phone=phone,birthday=birthday,pincode=pincode)
+        else:
+            gender = final_touch(gender)
+	
+        
+        if(not aadhaar):
+            flag_error=1
+            return render_template('registration.html',error_display_aadhaar="block",
+                    error_aadhaar="Error: Aadhaar UID is required",
+                    firstname=firstname,lastname=lastname,gender=gender,phone=phone,birthday=birthday,pincode=pincode)
+        else:
+            aadhaar = final_touch(aadhaar)
+            if(len(aadhaar)<12):
+                flag_error=1
+                return render_template('registration.html',error_display_aadhaar="block",
+                    error_aadhaar="Error: Aadhaar UID must be 12 digits long",
+                    firstname=firstname,lastname=lastname,gender=gender,phone=phone,birthday=birthday,pincode=pincode)
+
+        
+        if(not phone):
+            flag_error=1
+            return render_template('registration.html',error_display_phone="block",
+                    error_phone="Error: Phone No. is required",
+                    firstname=firstname,lastname=lastname,gender=gender,aadhaar=aadhaar,birthday=birthday,pincode=pincode)
+        else:
+            phone = final_touch(phone)
+            if(len(phone)<10):
+                flag_error=1
+                return render_template('registration.html',error_display_phone="block",
+                    error_phone="Error: Phone No. must be 10 digits long",
+                    firstname=firstname,lastname=lastname,gender=gender,aadhaar=aadhaar,birthday=birthday,pincode=pincode)
+
+        
+        if(not birthday):
+            flag_error=1
+            return render_template('registration.html',error_display_dob="block",
+                    error_dob="Error: Date fo Birth is required",
+                    firstname=firstname,lastname=lastname,gender=gender,aadhaar=aadhaar,phone=phone,pincode=pincode)
+        else:
+            birthday = final_touch(birthday)
+
+        
+        if(not pincode):
+            flag_error=1
+            return render_template('registration.html',error_display_pincode="block",
+                    error_pincode="Error: Pincode. is required",
+                    firstname=firstname,lastname=lastname,gender=gender,aadhaar=aadhaar,phone=phone,birthday=birthday)
+        else:
+            pincode = final_touch(pincode)
+            if(len(pincode)<6):
+                flag_error=1
+                return render_template('registration.html',error_display_pincode="block",
+                    error_pincode="Error: Pincode. must be 10 digits long",
+                    firstname=firstname,lastname=lastname,gender=gender,aadhaar=aadhaar,phone=phone,birthday=birthday)
 
 
-        firstname.capitalize()
-        lastname.capitalize()
+        if(flag_error==0):
+            error_display_name=error_display_username=error_display_password=error_display_gender =error_display_dob=error_display_head="none"
+        if(flag_error==1):
+            return render_template('registration.html',error_display_head="block",
+                    error_head="There are one or more errors in your form. Correct them and register again !",
+                    firstname=firstname,lastname=lastname,gender=gender,aadhaar=aadhaar,phone=phone,birthday=birthday,pincode=pincode)
+
+
 
     making_global_aadhaar(aadhaar)
 
@@ -115,7 +257,7 @@ def register():
 
         for j in uid:
             if j.get('aadhaar_uid')==aadhaar:
-                return render_template('registration.html',error="already a registered user ") 
+                return render_template('registration.html',error="Error : Already a registered user ",firstname=firstname,lastname=lastname,gender=gender) 
             else: 
                 id=random_n_digits(14)     
                 cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
@@ -126,6 +268,7 @@ def register():
                         
     else:
         id=random_n_digits(14)
+        cursor = mysql.connection.cursor(MySQLdb.cursors.DictCursor)
         cursor.execute('INSERT INTO patient VALUES (%s,% s, % s, % s, % s, % s, % s, % s,%s,%s)',(id,firstname, lastname, gender , aadhaar, phone, birthday, pincode, total, screening))
         mysql.connection.commit()
 
@@ -193,29 +336,6 @@ def ncd_rac():
             print("Please answer all the questions.",e)
             return render_template('question.html',error="please answer all the questions",patient_id=patient_id)         
         
-        
-        """if (age ==-1):
-            return redirect(url_for('fail'))
-        
-        smoke = int(request.form['smoke'])
-        if (smoke == -1):
-            return redirect(url_for('fail'))
-
-        alcohol = int(request.form['smoke'])
-        if (alcohol == -1):
-            return redirect(url_for('fail'))
-
-        waist = int(request.form['waist'])
-        if (waist ==-1):
-            return redirect(url_for('fail'))
-
-        phy_act = int(request.form['phy_act'])
-        if (phy_act == -1):
-            return redirect(url_for('fail'))
-
-        fam_his = int(request.form['fam_his'])
-        if (fam_his == -1):
-            return redirect(url_for('fail'))"""   
 
         total = age + smoke + alcohol + waist + phy_act + fam_his
 
@@ -236,5 +356,3 @@ def ncd_rac():
 
     return render_template('result.html',result=res,total=total, age=age,smoke=smoke,alcohol=alcohol,
                             waist=waist,phy_act=phy_act,fam_his=fam_his)              
-    
-
